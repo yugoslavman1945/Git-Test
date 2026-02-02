f14ce09fbbfe8f623c958efb80e679174b8b6a9b
Drew Smothers <dsmothe3@ut.utm.edu>
2026-02-02 13:54:42 -0600

    edit of main.cpp for Sum method

--- main.cpp
+++ main.cpp
@@ -4,4 +4,13 @@ using namespace std;
 int main()
 {
   cout << "Hello World!" << endl;
+  int x;
+  cout << "Enter an integer: ";
+  cin >> x;
+
+  for (int i = 1; i<=x; ++i)
+  {
+      sum+=i;
+  }
+  cout << "The sum of integers from 1 to X is: " << sum << endl;
 }
