40be1e6d89c380ed0f9b9fb3733705e4f134d05a
Drew Smothers <dsmothe3@ut.utm.edu>
2026-02-02 20:41:01 -0600

    main.cpp but now has multiplication and sum

--- main.cpp
+++ main.cpp
@@ -8,9 +8,14 @@ int main()
   cout << "Enter an integer: ";
   cin >> x;
 
+  int sum = 0;
+  int product = 1;
+  
   for (int i = 1; i<=x; ++i)
   {
       sum+=i;
+      product*=i;
   }
   cout << "The sum of integers from 1 to X is: " << sum << endl;
+  cout << "The product of integers from 1 to X is: " << product << endl;
 }
